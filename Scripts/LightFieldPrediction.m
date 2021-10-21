e960ed355b180efc89401677c3e5fb590c0d7055
Igor Rosler <idsrosler@inf.ufpel.edu.br>
2021-10-20 23:22:43 -0300

    Organizando a classe LightField

--- Scripts/LightFieldPrediction.m
+++ Scripts/LightFieldPrediction.m
@@ -1,8 +1,9 @@
-%% Ler Light Field no formato .ppm
-[LF, lf_name] = LFReadPPM();
-%% Criar um objeto de um Light Field 5D LF(x,y,s,t,c)
-L = LightField(lf_name, LF, '', 'PPM', '444', 'rgb');
-%% Teste
+%% Criar um objeto de um Light Field 5D LF(s,t,x,y,c)
+lf_path = '/home/idsrosler/Documentos/git/CTC_Datasets/Lenslets/Bikes/Bikes/';
+L = LightField(lf_path);
+
+%% Light Field Prediction
+clc; close all;
 disp(' ');
 disp('Showing the center sub-aperture image (SAI/View):');
 disp('>> imshow(L.getCenterSai());');
@@ -14,7 +15,7 @@ close all;
 disp(' ');
 disp('Converting the LightField to YCbCr color space:');
 disp('>> L.convert2ycbcr;');
-L.convert2ycbcr;
+L.convertToYCbCr;
 
 disp(' ');
 disp('Showing the center sub-aperture image (SAI/View):');
@@ -31,11 +32,11 @@ disp('...press enter to continue...');
 pause;
 close all;
 
-L.convert2rgb;
+L.convertToRGB;
 disp(' ');
 disp('Showing the micro image at spacial position x=500 and y=180:');
 disp('>> imshow(L.getMI(500,180));');
-imshow(L.getMI(500,180));
+imshow(L.getMicroImageXY(500,180));
 disp('...press enter to continue...');
 pause;
 figure;
@@ -51,7 +52,7 @@ close all;
 disp(' ');
 disp('It is also possible to see the LightField as a 2D matrix of micro images:');
 disp('>> imshow(L.get2dLf();');
-imshow(L.get2dLf());
+imshow(L.getMatrixMicroImage());
 disp('In order to enhance visualization of micro images, zooming in is recommended.');
 disp('...press enter to continue...');
 pause;
