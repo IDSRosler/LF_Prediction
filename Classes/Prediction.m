b3800c327c335740df346d755729d2e690cdc06c
Igor Rosler <idsrosler@inf.ufpel.edu.br>
2021-10-30 23:28:01 -0300

    Adição dos modos de predição Vertical, Horizontal e DC

--- Classes/Prediction.m
+++ Classes/Prediction.m
@@ -3,16 +3,14 @@ classdef Prediction < handle
     properties
         predictedLF; % Predicted Light Field
         predictedLFC; % Predicted Light Field into blocks 15x15
+        mode; % predict mode used to block (0:vertical|1:horizontal|2:DC|-1:block-copy)
     end
   
     %% Public Methods and Constructor
     methods
         %Constructor
-        function obj = Prediction(rows, columns, color)
-            obj.predictedLF = zeros(rows, columns, color);
-            
-            blockSizeR = 15 * 15; % Linhas no bloco (15 MIs de 15 pixels cada)
-            blockSizeC = 15 * 15; % Colunas no bloco (15 MIs de 15 pixels cada)
+        function obj = Prediction(rows, columns, color, blockSizeR, blockSizeC)
+            obj.predictedLF = uint16(zeros(rows, columns, color));
             
             wholeBlockRows = floor(rows / blockSizeR);
             blockR = [blockSizeR * ones(1, wholeBlockRows), rem(rows, blockSizeR)];
@@ -24,8 +22,55 @@ classdef Prediction < handle
         end
         
         % Predict block
-        function PredictBlock(obj, block, i, j)
-            obj.predictedLFC{i,j} = block;
+        function [blockMSE, predMode] = PredictBlock(obj, block, i, j)
+            mseBlock = 99999999999999999999;
+            if i == 1 || j == 1
+                blockP = block; 
+                mse = MSE(block, blockP);
+                if mse < mseBlock
+                   mseBlock = mse;
+                   obj.mode = -1;
+                end
+            else
+                refA = obj.AboveReference(i,j);
+                refL = obj.LeftReference(i,j);
+                
+                for m = 0:2
+                   switch m
+                       case 0
+                           if refA ~= -1
+                             blockP = obj.VerticalPrediction(refA, i, j); % vertical mode
+                           end
+                       case 1
+                           if refL ~= -1
+                             blockP = obj.HorizontalPrediction(refL, i, j); % horizontal mode
+                           end
+                       case 2
+                           if refA ~= -1
+                               if refL ~= -1
+                                 blockP = obj.DCPrediction(refA, refL, i, j); % DC mode
+                               end
+                           end
+                   end
+                   mse = MSE(block, blockP);
+                    if mse < mseBlock
+                       mseBlock = mse;
+                       obj.mode = m;
+                    end
+                end
+                
+                switch obj.mode
+                   case 0
+                       blockP = obj.VerticalPrediction(refA, i, j); % vertical mode
+                   case 1
+                       blockP = obj.HorizontalPrediction(refL, i, j); % horizontal mode
+                   case 2
+                       blockP = obj.DCPrediction(refA, refL, i, j); % DC mode
+                end
+            end
+            obj.predictedLFC{i,j} = blockP;
+            blockMSE = mseBlock;
+            predMode = obj.mode;
         end
         
         % Get predicted Light Field
@@ -36,6 +81,7 @@ classdef Prediction < handle
     
     %% Protected Methods
     methods (Access = protected)  
+        %% Reference Geters
         % Get above reference
         function A = AboveReference(obj, i, j)
             if i-1 > 0
@@ -61,6 +107,40 @@ classdef Prediction < handle
             else
                 L = -1;
             end
+        end
+        %% Predictors      
+        % Vertical prediction
+        function block = VerticalPrediction(obj, refA, i, j)
+            row_MI = refA(end-14:end, :, :);
+            x = floor(size(obj.predictedLFC{i,j}, 1)/15);
+            block = repmat(row_MI, [x, 1]); % replica a matriz de MIs no bloco a ser predito
+        end
+        
+        % Horizontal prediction
+        function block = HorizontalPrediction(obj, refL, i, j)
+            col_MI = refL(:,end-14:end, :);
+            y = floor(size(obj.predictedLFC{i,j}, 2)/15);
+            block = repmat(col_MI, [1, y]); % replica a matriz de MIs no bloco a ser predito
+        end
+        
+        % DC prediction
+        function block = DCPrediction(obj, refA, refL, i, j)
+            row_MI = refA(end-14:end, :, :);
+            col_MI = refL(:,end-14:end, :);
+            col_MI = permute(col_MI, [2, 1, 3]); % transforma uma coluna de MIs em uma linhas de MIs
+            ry  = size(row_MI, 2);
+            cx = size(col_MI, 2);
+            if ry < cx % verifica se a referência de cima é menor que a referência a esquerda para poder calcular a média e não ocorrer problemas de indexação
+                me = floor((row_MI + col_MI(:, 1:ry, :))./2);
+            elseif ry > cx % verifica se a referência de cima é maior que a referência a esquerda para poder calcular a média e não ocorrer problemas de indexação
+                me = uint16(zeros(15, ry, 3));
+                me(:, 1:cx, :) = floor((row_MI(:, 1:cx, :) + col_MI)./2);
+                me(:, cx+1:ry, :) = row_MI(:, cx+1:ry, :);
+            else
+                me = floor((row_MI + col_MI)./2);
+            end
+            x = floor(size(obj.predictedLFC{i,j}, 1)/15);
+            block = repmat(me, [x, 1]); % replica a matriz de MIs no bloco a ser predito
         end
     end
 end
--- Scripts/LightFieldPrediction.m
+++ Scripts/LightFieldPrediction.m
@@ -8,10 +8,10 @@ clc; close all;
 lenslet = L.getLensletFormat;
 [rows, columns, c] = size(lenslet);
 
-prediction = Prediction(rows, columns, c);
-
 blockSizeR = 15 * 15; % Linhas no bloco (15 MIs de 15 pixels cada)
 blockSizeC = 15 * 15; % Colunas no bloco (15 MIs de 15 pixels cada)
+
+prediction = Prediction(rows, columns, c, blockSizeR, blockSizeC);
 
 wholeBlockRows = floor(rows / blockSizeR);
 blockR = [blockSizeR * ones(1, wholeBlockRows), rem(rows, blockSizeR)];
@@ -25,8 +25,8 @@ ca = mat2cell(lenslet, blockR, blockC, c); % Divide o LF em blocos 15x15
 for i = 1:r
     for j = 1:c
         block = ca{i,j};
-        prediction.PredictBlock(block, i, j);
-        disp(strcat('block = {x: ', int2str(i), ', y: ', int2str(j),'}'));
+        [blockMSE, mode] = prediction.PredictBlock(block, i, j);
+        disp(strcat('Block: {x: ', int2str(i), ', y: ', int2str(j),'} | MSE: ', num2str(blockMSE), ' | mode: ', int2str(mode)));
     end
 end
 
@@ -43,5 +43,5 @@ disp(strcat('U_PSNR: ', num2str(u_psnr)));
 disp(strcat('V_PSNR: ', num2str(v_psnr)));
 disp(strcat('YUV_PSNR: ', num2str(yuv_psnr)));
 
-% figure(1); imshow(lenslet); title('Original LF');
-% figure(2); imshow(predictedLF); title('Predicted LF');
+figure(1); imshow(lenslet); title('Original LF');
+figure(2); imshow(predictedLF); title('Predicted LF');
